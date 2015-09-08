251b8a17c0e3356b47d8f5176c7b83996e6c849f
Marc Auger <sosaysthecaptain@gmail.com>
2015-09-07 22:38:46 -0500

    special event work. Still need to figure out several values

--- SpaceTrader/SystemDataTypes.swift
+++ SpaceTrader/SystemDataTypes.swift
@@ -92,3 +92,54 @@ let StateOfSatori = PoliticsType(name: "State of Satori", index: 14, reactionIll
 let Technocracy = PoliticsType(name: "Technocracy", index: 15, reactionIllegal: 1, activityPolice: 6, activityPirates: 3, activityTraders: 6, minTech: 4, maxTech: 7, bribeLevel: 2, drugsOk: true, firearmsOk: true, wanted: "Water")
 let Theocracy = PoliticsType(name: "Theocracy", index: 16, reactionIllegal: 5, activityPolice: 6, activityPirates: 1, activityTraders: 4, minTech: 0, maxTech: 4, bribeLevel: 0, drugsOk: true, firearmsOk: true, wanted: "Narcotics")
 
+class SpecialEventType {
+    let header: String
+    let text: String
+    let cost: Int
+    // second number?
+    // bool?
+    
+    init(header: String, text: String, cost: Int) {
+        self.header = header
+        self.text = text
+        self.cost = cost
+    }
+}
+
+//    { "Dragonfly Destroyed",	QuestDragonflyDestroyedString,		0, 0, true },
+//    { "Weird Ship",				QuestWeirdShipString,				0, 0, true },
+//    { "Lightning Ship",			QuestLightningShipString,			0, 0, true },
+//    { "Strange Ship",			QuestStrangeShipString,				0, 0, true },
+//    { "Monster Killed", 		QuestMonsterKilledString, 	   -15000, 0, true },
+//    { "Medicine Delivery", 		QuestMedicineDeliveredString,		0, 0, true },
+//    { "Retirement",     		QuestRetirementString,				0, 0, false },
+//    { "Moon For Sale",  		QuestMoonForSaleString, 	 COSTMOON, 4, false },
+//    { "Skill Increase", 		QuestSkillIncreaseString,		 3000, 3, false },
+//    { "Merchant Prince",		QuestMerchantPrinceString,		 1000, 1, false },
+//    { "Erase Record",   		QuestEraseRecordString,			 5000, 3, false },
+//    { "Tribble Buyer",  		QuestTribbleBuyerString, 			0, 3, false },
+//    { "Space Monster",  		QuestSpaceMonsterString, 			0, 1, true },
+//    { "Dragonfly",      		QuestDragonflyString, 				0, 1, true },
+//    { "Cargo For Sale", 		QuestCargoForSaleString, 	 	 1000, 3, false },
+//    { "Lightning Shield", 		QuestLightningShieldString,	 		0, 0, false },
+//    { "Japori Disease", 		QuestJaporiDiseaseString, 			0, 1, false },
+//    { "Lottery Winner", 		QuestLotteryWinnerString, 		-1000, 0, true },
+//    { "Artifact Delivery", 		QuestArtifactDeliveryString,	-20000, 0, true },
+//    { "Alien Artifact", 		QuestAlienArtifactString, 			0, 1, false },
+//    { "Ambassador Jarek", 		QuestAmbassadorJarekString,		 	0, 1, false },
+//    { "Alien Invasion",			QuestAlienInvasionString,		 	0, 0, true },
+//    { "Gemulon Invaded", 		QuestGemulonInvadedString, 			0, 0, true },
+//    { "Fuel Compactor", 		QuestFuelCompactorString, 			0, 0, false },
+//    { "Dangerous Experiment",   QuestDangerousExperimentString,		0, 0, true },
+//    { "Jonathan Wild",  		QuestJonathanWildString, 			0, 1, false },
+//    { "Morgan's Reactor",  		QuestMorgansReactorString,	 		0, 0, false },
+//    { "Install Morgan's Laser", QuestInstallMorgansLaserString,	 	0, 0, false },
+//    { "Scarab Stolen", 		QuestScarabStolenString,		 	0, 1, true},
+//    { "Upgrade Hull", 			QuestUpgradeHullString, 			0, 0, false},
+//    { "Scarab Destroyed", 	QuestScarabDestroyedString,	 	0, 0, true},
+//    { "Reactor Delivered",  	QuestReactorDeliveredString, 		0, 0, true },
+//    { "Jarek Gets Out",			QuestJarekGetsOutString, 			0, 0, true },
+//    { "Gemulon Rescued", 		QuestGemulonRescuedString,	 		0, 0, true },
+//    { "Disaster Averted",		QuestDisasterAvertedString,			0, 0, true },
+//    { "Experiment Failed",		QuestExperimentFailedString, 		0, 0, true },
+//    { "Wild Gets Out",          QuestWildGetsOutString,
