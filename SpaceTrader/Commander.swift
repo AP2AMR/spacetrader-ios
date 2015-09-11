6183148a5166a82d22976a8ae9ed56f46b13c8f7
Marc Auger <sosaysthecaptain@gmail.com>
2015-09-11 14:37:56 -0500

    commander class created in most basic form

--- SpaceTrader/Commander.swift
+++ SpaceTrader/Commander.swift
@@ -9,5 +9,101 @@
 import Foundation
 
 class Commander {
+    var commanderName: String
+    var difficulty: DifficultyType
+    var commanderShip: SpaceShip
+    //var currentSystem: StarSystemID
+    var credits: Int
+    var debt: Int = 0
+    var days: Int = 0
     
+    // skills
+    var pilotSkill: Int
+    var fighterSkill: Int
+    var traderSkill: Int
+    var engineerSkill: Int
+    
+    init(commanderName: String, difficulty: DifficultyType, pilotSkill: Int, fighterSkill: Int, traderSkill: Int, engineerSkill: Int) {
+        self.commanderName = commanderName
+        self.difficulty = difficulty
+        self.pilotSkill = pilotSkill
+        self.fighterSkill = fighterSkill
+        self.traderSkill = traderSkill
+        self.engineerSkill = engineerSkill
+        
+        self.credits = 1000
+        
+        self.commanderShip = SpaceShip(type: ShipType.Flea, IFFStatus: IFFStatusType.Player)
+    }
+    
+    // current prices
+    
+    // settings
+    
+    
+//    var Credits: Int
+//    var Debt: Int
+//    var Days: Int
+//    let CurrentSystem: StarSystemID
+//    let BuyPrice: Int
+//    let SellPrice: Int
+//    let ShipPrice: Int
+//    let GalacticChartSystem: Int                    // ?
+//    let PoliceKills: Int
+//    let TraderKills: Int
+//    let PirateKills: Int
+//    let PoliceRecordScore: Int
+//    let AutoFuel: Bool
+//    let AutoRepair: Bool
+//    let Clicks: Bool                                // ?
+//    let EncounterType: EncounterType2               // wtf?
+//    let Raided: Bool                                // ?
+//    let MonsterStatus: MonsterStatusType
+//    let DragonflyStatus: DragonflyStatusType
+//    let JaporiDiseaseStatus: JaporiDiseaseStatusType
+//    let MoonBought: MoonBoughtStatus
+//    let MonsterHull: Int                            // what's this?
+//    let NameCommander: String
+//    let CurrentForm: Int                            // what's this?
+//    let Ship: SpaceShip
+//    let Opponent: SpaceShip
+//    let Mercenary: [CrewMember]
+//    let StarSystem: StarSystemID
+//    let EscapePod: Bool
+//    let Insurance: Bool
+//    let NoClaim: Int                                // what's this?
+//    let Inspected: Bool
+//    let AlwaysIgnoreTraders: Bool                   // maybe a separate section of settings?
+//    let Wormhole: Int                               // NOT INT BUT I HAVE NO IDEA
+//    let Difficulty: DifficultyType
+//    //let VersionMajor                                // what's this?
+//    //let VersionMinor                                // what's this?
+//    // buying price is to be included in specific tradeitems? Else must go here.
+//    let ArtifactOnBoard: Bool
+//    let ReserveMoney: Bool                          // ?
+//    let PriceDifferences: Bool                      // ?
+//    let APLScreen: Bool                             // ?
+//    let LeaveEmpty: Int                             // bays?
+//    let TribbleMessage: Bool
+//    let AlwaysInfo: Bool
+//    let AlwaysIgnorePolice: Bool
+//    let AlwaysIgnorePirates: Bool
+//    // textual encounters
+//    let JarekStatus: JarekStatusType
+//    // continuous: bool?
+//    let AttackFleeing: Bool
+//    let ExperimentAndWildStatus: ExperimentAndWildStatusType
+//    let FabricRipProbability: FabricRipProbabilityType
+//    let VeryRareEncounter: VeryRareEncounterType
+//    // boolean collection?
+//    let ReactorStatus: ReactorStatusType
+//    let TrackedSystem: StarSystemID
+//    let ScarabStatus: ScarabStatusType
+//    let AlwaysIgnoreTradeInOrbit: Bool
+//    let AlreadyPaidForNewspaper: Bool
+//    let GameLoaded: Bool                            // ?
+//    let LitterWarning: Bool
+//    let SharePreferences: Bool
+    
+    // preferences
 }
