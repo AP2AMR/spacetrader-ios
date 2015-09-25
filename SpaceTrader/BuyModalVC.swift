001a5c0c21aec15b23a8c15b746ed7c561ed5cd1
Marc Auger <sosaysthecaptain@gmail.com>
2015-09-24 22:32:25 -0500

    buy modal in good shape. About to set up delegate to tell it which link called it

--- SpaceTrader/BuyModalVC.swift
+++ SpaceTrader/BuyModalVC.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class BuyModalVC: UIViewController {
 
+    var max = 5
+    
+    @IBOutlet weak var quantityField: UITextField!
+        
+    override func viewDidLoad() {
+        quantityField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        self.dismissViewControllerAnimated(false, completion: nil)
+    }
+   
+    @IBAction func buy() {
+        if quantityField.text != nil {
+            let quantity = Int(quantityField.text!)
+            if quantity <= max {
+                print("buy \(quantity!) items")
+                self.dismissViewControllerAnimated(false, completion: nil)
+            } else {
+                self.dismissViewControllerAnimated(false, completion: nil)
+            }
+        }
+    }
+    
+    
 }
