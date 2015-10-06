69912691027be7c067b1f3278bd92e8733323dc6
Marc Auger <sosaysthecaptain@gmail.com>
2015-10-06 10:11:15 -0500

    significant progress on drawing planets in range on short range chart

--- SpaceTrader/ShortRangeChartView.swift
+++ SpaceTrader/ShortRangeChartView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ShortRangeChartView: UIView {
-    let pointsPerClick: CGFloat = 5
+    let pointsPerParsec: CGFloat = 5
     let circleColor = UIColor.blackColor()
     
     var locationOfCurrentPlanet: CGPoint {
@@ -19,7 +19,7 @@ class ShortRangeChartView: UIView {
     }
     
     var rangeCircleRadius: CGFloat {
-        return CGFloat(player.commanderShip.fuelTanks) * pointsPerClick
+        return CGFloat(player.commanderShip.fuelTanks) * pointsPerParsec
     }
     
     override func drawRect(rect: CGRect) {
@@ -28,5 +28,67 @@ class ShortRangeChartView: UIView {
         circleColor.set()
         rangeCirclePath.stroke()
         
+        // draw current planet
+        drawPlanetCircle(locationOfCurrentPlanet, visited: true)
+        
+        let x = galaxy.currentSystem!.xCoord + 5
+        let y = galaxy.currentSystem!.yCoord + 7
+        
+        let testPlanet = StarSystem(name: "test", techLevel: TechLevelType.techLevel4, politics: PoliticsType.socialist, status: StatusType.none, xCoord: x, yCoord: y, specialResources: SpecialResourcesType.none, size: SizeType.Small)
+        drawPlanet(testPlanet)
+        
+        for system in systemsInRange {
+            drawPlanet(system)
+        }
+        
+    }
+    
+    
+    
+    func drawPlanet(system: StarSystem) {
+        let currentSystemMapXCoord: CGFloat = locationOfCurrentPlanet.x
+        let currentSystemMapYCoord: CGFloat = locationOfCurrentPlanet.y
+        
+        let xDifference: CGFloat = CGFloat(galaxy.currentSystem!.xCoord) - CGFloat(system.xCoord)
+        let yDifference: CGFloat = CGFloat(galaxy.currentSystem!.yCoord) - CGFloat(system.yCoord)
+        
+        let xCoord: CGFloat = currentSystemMapXCoord + (xDifference * CGFloat(pointsPerParsec))
+        let yCoord: CGFloat = currentSystemMapYCoord + (yDifference * CGFloat(pointsPerParsec))
+        
+        print("************************************************************")
+        print("second planet: \(system.name)")
+        print("DEBUG DRAW currentSystem: galaxy xCoord: \(galaxy.currentSystem!.xCoord)")
+        print("DEBUG DRAW currentSystem: galaxy yCoord: \(galaxy.currentSystem!.yCoord)")
+        print("DEBUG DRAW currentSystem: map xCoord: \(currentSystemMapXCoord)")
+        print("DEBUG DRAW currentSystem: map yCoord: \(currentSystemMapYCoord)")
+        
+        print("DEBUG DRAW second planet: galaxy xCoord: \(system.xCoord)")
+        print("DEBUG DRAW second planet: galaxy yCoord: \(system.yCoord)")
+        print("DEBUG DRAW second planet: map xCoord: \(xCoord)")
+        print("DEBUG DRAW second planet: map yCoord: \(yCoord)")
+
+        let location = CGPointMake(xCoord, yCoord)
+        let visited: Bool = system.visited
+        
+        drawPlanetCircle(location, visited: visited)
+    }
+    
+    // make function to draw planet, given StarSystem. Draws from planet info to determine if visited or has wormhole. Early version could take coordinates, specs from arguments.
+    // locations are relative to current planet
+    func drawPlanetCircle(location: CGPoint, visited: Bool) {
+        let planetRadius = CGFloat(4)
+        //let location = CGPointMake(xCoord, yCoord)
+        let planetCircle = UIBezierPath(arcCenter: location, radius: planetRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        let unvisitedColor: UIColor = UIColor.greenColor()
+        let visitedColor: UIColor = UIColor.blueColor()
+        
+        if visited == true {
+            visitedColor.set()
+        } else {
+            unvisitedColor.set()
+        }
+        
+        planetCircle.stroke()
+        planetCircle.fill()
     }
 }
