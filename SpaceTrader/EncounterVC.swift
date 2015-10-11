46e881532f0d4495b900cd6d6ee951cc978c4778
Marc Auger <sosaysthecaptain@gmail.com>
2015-10-10 22:43:19 -0500

    added new modal

---
+++ SpaceTrader/EncounterVC.swift
@@ -0,0 +1,22 @@
+//
+//  EncounterVC.swift
+//  SpaceTrader
+//
+//  Created by Marc Auger on 10/10/15.
+//  Copyright © 2015 Marc Auger. All rights reserved.
+//
+
+import UIKit
+
+class EncounterVC: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
