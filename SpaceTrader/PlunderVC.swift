67b7ed6eb2b32e1705a83a7c589e1cf03db72875
Marc Auger <sosaysthecaptain@gmail.com>
2015-10-31 16:50:36 -0500

    progress on plunderVC. About to do computed properties for quantities on ship

--- SpaceTrader/PlunderVC.swift
+++ SpaceTrader/PlunderVC.swift
@@ -15,7 +15,26 @@ class PlunderVC: UIViewController {
         self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBOutlet weak var waterQuantity: UIButton!
+    @IBOutlet weak var fursQuantity: UIButton!
+    @IBOutlet weak var foodQuantity: UIButton!
+    @IBOutlet weak var oreQuantity: UIButton!
+    @IBOutlet weak var gamesQuantity: UIButton!
+    @IBOutlet weak var firearmsQuantity: UIButton!
+    @IBOutlet weak var medicineQuantity: UIButton!
+    @IBOutlet weak var machinesQuantity: UIButton!
+    @IBOutlet weak var narcoticsQuantity: UIButton!
+    @IBOutlet weak var robotsQuantity: UIButton!
 
-
+    @IBOutlet weak var baysLabel: UILabel!
+    
+    func updateUI() {
+        let controlState = UIControlState()
+        
+        // set quantities
+        waterQuantity.setTitle("\(galaxy.currentSystem!.water)", forState: controlState)
+    }
 
 }
