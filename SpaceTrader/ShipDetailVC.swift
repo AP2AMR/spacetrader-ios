0470c080a13af626146a09863fedcfbb80f203dd
Marc Auger <sosaysthecaptain@gmail.com>
2015-11-23 13:46:15 -0600

    shipDetail: view done, labels connected

--- SpaceTrader/ShipDetailVC.swift
+++ SpaceTrader/ShipDetailVC.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ShipDetailVC: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var cargoLabel: UILabel!
+    @IBOutlet weak var rangeLabel: UILabel!
+    @IBOutlet weak var hullLabel: UILabel!
+    @IBOutlet weak var weaponLabel: UILabel!
+    @IBOutlet weak var shieldLabel: UILabel!
+    @IBOutlet weak var gadgetLabel: UILabel!
+    @IBOutlet weak var crewLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
