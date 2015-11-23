b45a97dac873fdf3301cea7d1353707db0733f04
Marc Auger <sosaysthecaptain@gmail.com>
2015-11-23 12:08:05 -0600

    adding tableView, doesn't work yet

--- SpaceTrader/ShipListVC.swift
+++ SpaceTrader/ShipListVC.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class ShipListVC: UIViewController {
+class ShipListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var items: [String] = ["Flea", "Gnat", "Firefly", "Mosquito", "Bumblebee", "Beetle", "Hornet", "Grasshopper", "Termite", "Wasp"]
+    
+//    case Flea
+//    case Gnat
+//    case Firefly
+//    case Mosquito
+//    case Bumblebee
+//    case Beetle
+//    case Hornet
+//    case Grasshopper
+//    case Termite
+//    case Wasp
+//    case Custom
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.registerClass(UITableViewCell.self, forHeaderFooterViewReuseIdentifier: "cell")
     }
     
     @IBAction func done(sender: AnyObject) {
@@ -21,6 +37,20 @@ class ShipListVC: UIViewController {
     }
     
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
 
 }
