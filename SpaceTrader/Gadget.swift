18c5936aeb6c5bd5ee8989979240ee73e74fe092
Marc Auger <sosaysthecaptain@gmail.com>
2015-11-25 22:04:49 -0600

    sell prices for gadgets added

--- SpaceTrader/Gadget.swift
+++ SpaceTrader/Gadget.swift
@@ -12,6 +12,7 @@ class Gadget {
     let type: GadgetType
     let name: String
     let price: Int
+    let sellPrice: Int
     let techLevel: TechLevelType
     let chance: Int
 
@@ -21,27 +22,32 @@ class Gadget {
         switch type {
         case GadgetType.CargoBays:
             self.name = "5 extra cargo bays"
-            self.price = 2500
+            self.price = 2475
+            self.sellPrice = 1875
             self.techLevel = TechLevelType.techLevel4
             self.chance = 35
         case GadgetType.AutoRepair:
             self.name = "Auto-repair system"
-            self.price = 7500
+            self.price = 7450
+            self.sellPrice = 5625
             self.techLevel = TechLevelType.techLevel5
             self.chance = 20
         case GadgetType.Navigation:
             self.name = "Navigating system"
-            self.price = 15000
+            self.price = 14850
+            self.price = 11250
             self.techLevel = TechLevelType.techLevel6
             self.chance = 20
         case GadgetType.Targeting:
             self.name = "Targeting system"
-            self.price = 25000
+            self.price = 24750
+            self.sellPrice = 18750
             self.techLevel = TechLevelType.techLevel6
             self.chance = 20
         case GadgetType.Cloaking:
             self.name = "Cloaking device"
-            self.price = 100000
+            self.price = 99000
+            self.sellPrice = 75000
             self.techLevel = TechLevelType.techLevel7
             self.chance = 5
         case GadgetType.FuelCompactor:
--- SpaceTrader/Shield.swift
+++ SpaceTrader/Shield.swift
@@ -14,6 +14,7 @@ class Shield {
     let power: Int
     var currentStrength: Int = 0    // do we want to initialize them at zero? I think so?
     let price: Int
+    let sellPrice: Int
     let techLevel: TechLevelType
     let chance: Int                 // percent chance that this is fitted in a slot
     
@@ -33,19 +34,22 @@ class Shield {
         case ShieldType.energyShield:
             self.name = "Energy shield"
             self.power = 100
-            self.price = 5000
+            self.price = 4950
+            self.sellPrice = 3750
             self.techLevel = TechLevelType.techLevel5
             self.chance = 70
         case ShieldType.reflectiveShield:
             self.name = "Reflective shield"
             self.power = 200
-            self.price = 20000
+            self.price = 19800
+            self.sellPrice = 15000
             self.techLevel = TechLevelType.techLevel6
             self.chance = 30
         case ShieldType.lightningShield:
             self.name = "Lightning shield"
             self.power = 350
             self.price = 45000
+            self.sellPrice = 0              // *****
             self.techLevel = TechLevelType.techLevel8
             self.chance = 0
         }
