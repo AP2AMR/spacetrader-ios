708bcdc9c7e62cd8be252c48c93ef3cfe57d9e01
Marc Auger <sosaysthecaptain@gmail.com>
2015-11-30 15:51:09 -0600

    progress toward kludgy buy/sell equipment

--- SpaceTrader/EquipmentListVC.swift
+++ SpaceTrader/EquipmentListVC.swift
@@ -10,17 +10,281 @@ import UIKit
 
 class EquipmentListVC: UIViewController {
 
+    var selectorIndex = 0
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    let universalArray: [UniversalGadgetType] = [UniversalGadgetType.pulseLaser, UniversalGadgetType.beamLaser, UniversalGadgetType.militaryLaser, UniversalGadgetType.photonDisruptor, UniversalGadgetType.energyShield, UniversalGadgetType.reflectiveShield, UniversalGadgetType.CargoBays, UniversalGadgetType.AutoRepair, UniversalGadgetType.Navigation, UniversalGadgetType.Targeting, UniversalGadgetType.Cloaking]
+    @IBOutlet weak var itemSlots: UILabel!
+    @IBOutlet weak var itemsAvalable: UILabel!
+    
+    @IBOutlet weak var slot1: CustomButton!
+    @IBOutlet weak var slot2: CustomButton!
+    @IBOutlet weak var slot3: CustomButton!
+    
+    @IBOutlet weak var item1: UIButton!
+    @IBOutlet weak var item2: UIButton!
+    @IBOutlet weak var item3: UIButton!
+    @IBOutlet weak var item4: UIButton!
+    @IBOutlet weak var item5: UIButton!
+    
+    
+    //let universalArray: [UniversalGadgetType] = [UniversalGadgetType.pulseLaser, UniversalGadgetType.beamLaser, UniversalGadgetType.militaryLaser, UniversalGadgetType.photonDisruptor, UniversalGadgetType.energyShield, UniversalGadgetType.reflectiveShield, UniversalGadgetType.CargoBays, UniversalGadgetType.AutoRepair, UniversalGadgetType.Navigation, UniversalGadgetType.Targeting, UniversalGadgetType.Cloaking]
+    
+    let weaponsArray: [WeaponType] = [WeaponType.pulseLaser, WeaponType.beamLaser, WeaponType.militaryLaser, WeaponType.photonDisruptor]
+    let shieldsArray: [ShieldType] = [ShieldType.energyShield, ShieldType.reflectiveShield]
+    let gadgetsArray: [GadgetType] = [GadgetType.CargoBays, GadgetType.AutoRepair, GadgetType.Navigation, GadgetType.Targeting, GadgetType.Cloaking]
+    
+    var availableWeapons: [Weapon] = []
+    var availableShields: [Shield] = []
+    var availableGadgets: [Gadget] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        populateAvailable()
+        populateSlots()
+        populateLabels()
+        
+    }
+    
+    
+
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                print("weapons selected")
+                selectorIndex = 0
+                populateLabels()
+                populateSlots()
+            case 1:
+                print("shields selected")
+                selectorIndex = 1
+                populateLabels()
+                populateSlots()
+            case 2:
+                print("gadgets selected")
+                selectorIndex = 2
+                populateLabels()
+                populateSlots()
+            default:
+                print("error")
+        }
     }
 
     @IBAction func done(sender: AnyObject) {
         self.dismissViewControllerAnimated(false, completion: nil)
     }
     
+    func populateLabels() {
+        if selectorIndex == 0 {
+            itemSlots.text = "Weapon Slots on Your Ship:"
+            itemsAvalable.text = "Weapons Available:"
+            
+            let controlState = UIControlState()
+            
+            item1.setTitle("", forState: controlState)
+            item2.setTitle("", forState: controlState)
+            item3.setTitle("", forState: controlState)
+            item4.setTitle("", forState: controlState)
+            item5.setTitle("", forState: controlState)
+            
+            if availableWeapons.count >= 1 {
+                item1.setTitle("\(availableWeapons[0].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 2 {
+                item2.setTitle("\(availableWeapons[1].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 3 {
+                item3.setTitle("\(availableWeapons[2].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 4 {
+                item4.setTitle("\(availableWeapons[3].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 5 {
+                item5.setTitle("\(availableWeapons[4].name)", forState: controlState)
+            }
+            
+        } else if selectorIndex == 1 {
+            itemSlots.text = "Shield Slots on Your Ship:"
+            itemsAvalable.text = "Shields Available:"
+            
+            let controlState = UIControlState()
+            
+            item1.setTitle("", forState: controlState)
+            item2.setTitle("", forState: controlState)
+            item3.setTitle("", forState: controlState)
+            item4.setTitle("", forState: controlState)
+            item5.setTitle("", forState: controlState)
+            
+            if availableWeapons.count >= 1 {
+                item1.setTitle("\(availableShields[0].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 2 {
+                item2.setTitle("\(availableShields[1].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 3 {
+                item3.setTitle("\(availableShields[2].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 4 {
+                item4.setTitle("\(availableShields[3].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 5 {
+                item5.setTitle("\(availableShields[4].name)", forState: controlState)
+            }
+            
+        } else if selectorIndex == 2 {
+            itemSlots.text = "Gadget Slots on Your Ship:"
+            itemsAvalable.text = "Gadgets Available:"
+            
+            let controlState = UIControlState()
+            
+            item1.setTitle("", forState: controlState)
+            item2.setTitle("", forState: controlState)
+            item3.setTitle("", forState: controlState)
+            item4.setTitle("", forState: controlState)
+            item5.setTitle("", forState: controlState)
+            
+            if availableWeapons.count >= 1 {
+                item1.setTitle("\(availableGadgets[0].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 2 {
+                item2.setTitle("\(availableGadgets[1].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 3 {
+                item3.setTitle("\(availableGadgets[2].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 4 {
+                item4.setTitle("\(availableGadgets[3].name)", forState: controlState)
+            }
+            if availableWeapons.count >= 5 {
+                item5.setTitle("\(availableGadgets[4].name)", forState: controlState)
+            }
+        }
+    }
+    
+    func populateAvailable() {
+        let currentSystemTechLevel = getTechLevelInt(galaxy.currentSystem!.techLevel)
+        
+        for item in weaponsArray {
+            let prototype = Weapon(type: item)
+            let neededTechLevel = getTechLevelInt(prototype.techLevel)
+            if neededTechLevel <= currentSystemTechLevel {
+                print("including \(prototype.name)")
+                availableWeapons.append(prototype)
+            }
+        }
+        
+        for item in shieldsArray {
+            let prototype = Shield(type: item)
+            let neededTechLevel = getTechLevelInt(prototype.techLevel)
+            if neededTechLevel <= currentSystemTechLevel {
+                print("including \(prototype.name)")
+                availableShields.append(prototype)
+            }
+        }
+        
+        for item in gadgetsArray {
+            let prototype = Gadget(type: item)
+            let neededTechLevel = getTechLevelInt(prototype.techLevel)
+            if neededTechLevel <= currentSystemTechLevel {
+                print("including \(prototype.name)")
+                availableGadgets.append(prototype)
+            }
+            
+        }
+    }
+    
+    func populateSlots() {
+        // set all titles to empty
+        let controlState = UIControlState()
+        slot1.setTitle("Slot 1: <empty>", forState: controlState)
+        slot2.setTitle("Slot 2: <empty>", forState: controlState)
+        slot3.setTitle("Slot 3: <empty>", forState: controlState)
+        
+        // get correct number of slots
+        var numberOfSlots = 0
+        switch selectorIndex {
+            case 0:
+                numberOfSlots = player.commanderShip.weaponSlots
+            case 1:
+                numberOfSlots = player.commanderShip.shieldSlots
+            case 2:
+                numberOfSlots = player.commanderShip.gadgetSlots
+            default:
+                print("error")
+        }
+        
+        // display correct number of slots
+        switch numberOfSlots {
+        case 0:
+            slot1.enabled = false
+            slot2.enabled = false
+            slot3.enabled = false
+        case 1:
+            slot1.enabled = true
+            slot2.enabled = false
+            slot3.enabled = false
+        case 2:
+            slot1.enabled = true
+            slot2.enabled = true
+            slot3.enabled = false
+        case 3:
+            slot1.enabled = true
+            slot2.enabled = true
+            slot3.enabled = true
+        default:
+            print("error")
+        }
+        
+        //
+        if selectorIndex == 0 {
+            let controlState = UIControlState()
+            
+            if player.commanderShip.weapon.count >= 1 {
+                slot1.setTitle("Slot 1: \(player.commanderShip.weapon[0].name)", forState: controlState)
+            }
+            if player.commanderShip.weapon.count >= 2 {
+                slot2.setTitle("Slot 2: \(player.commanderShip.weapon[1].name)", forState: controlState)
+            }
+            if player.commanderShip.weapon.count >= 3 {
+                slot3.setTitle("Slot 3: \(player.commanderShip.weapon[2].name)", forState: controlState)
+            }
+        } else if selectorIndex == 1 {
+            
+        } else if selectorIndex == 2 {
+            
+        }
+    }
+    
+    @IBAction func item1Button(sender: AnyObject) {
+        performSegueWithIdentifier("gadgetDetail", sender: nil)
+    }
+    
+    @IBAction func item2Button(sender: AnyObject) {
+        performSegueWithIdentifier("gadgetDetail", sender: nil)
+    }
+    
+    @IBAction func item3Button(sender: AnyObject) {
+        performSegueWithIdentifier("gadgetDetail", sender: nil)
+    }
+    
+    @IBAction func item4Button(sender: AnyObject) {
+        performSegueWithIdentifier("gadgetDetail", sender: nil)
+    }
+    
+    @IBAction func item5Button(sender: AnyObject) {
+        performSegueWithIdentifier("gadgetDetail", sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if(segue.identifier == "gadgetDetail") {
+            
+//            let vc = (segue.destinationViewController as! ShipDetailVC)
+//            vc.ship = chosenShip
+//            vc.typeOfShip = chosenShipType
+        }
+        
+    }
     
 }
