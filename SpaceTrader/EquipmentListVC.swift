a65534521bb1e44717e6960d3c04632eac0ec570
Marc Auger <sosaysthecaptain@gmail.com>
2015-11-30 13:34:14 -0600

    using buttons instead

--- SpaceTrader/EquipmentListVC.swift
+++ SpaceTrader/EquipmentListVC.swift
@@ -8,47 +8,19 @@
 
 import UIKit
 
-class EquipmentListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class EquipmentListVC: UIViewController {
 
-    @IBOutlet weak var availableTableView: UITableView!
-    
-    let weaponsArray: [WeaponType] = [WeaponType.pulseLaser, WeaponType.beamLaser, WeaponType.militaryLaser, WeaponType.photonDisruptor]
-    let shieldsArray: [ShieldType] = [ShieldType.energyShield, ShieldType.reflectiveShield]
-    let gadgetsArray: [GadgetType] = [GadgetType.CargoBays, GadgetType.AutoRepair, GadgetType.Navigation, GadgetType.Targeting, GadgetType.Cloaking]
     
     let universalArray: [UniversalGadgetType] = [UniversalGadgetType.pulseLaser, UniversalGadgetType.beamLaser, UniversalGadgetType.militaryLaser, UniversalGadgetType.photonDisruptor, UniversalGadgetType.energyShield, UniversalGadgetType.reflectiveShield, UniversalGadgetType.CargoBays, UniversalGadgetType.AutoRepair, UniversalGadgetType.Navigation, UniversalGadgetType.Targeting, UniversalGadgetType.Cloaking]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.availableTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "equipmentCell")
+        
     }
 
     @IBAction func done(sender: AnyObject) {
         self.dismissViewControllerAnimated(false, completion: nil)
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.universalArray.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: BuyEquipmentCell = self.availableTableView.dequeueReusableCellWithIdentifier("equipmentCell")! as! BuyEquipmentCell
-        //let cell: BuyShipCell = self.availableTableView.dequeueReusableCellWithIdentifier("equipmentCell")! as! BuyShipCell
-        let item = self.universalArray[indexPath.row]       // BUG LIVES HERE
-        cell.setCell(item)
-        print("setting cell at row \(indexPath.row)")
-        
-        return cell
-
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("You selected cell #\(indexPath.row)!")
-//        chosenShip = items[indexPath.row]
-//        chosenShipType = ships[indexPath.row]
-//        print("You selected \(chosenShip)")
-//        performSegueWithIdentifier("shipDetail", sender: chosenShip)
-    }
     
 }
