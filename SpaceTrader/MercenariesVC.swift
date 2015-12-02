9f02cfdf7118fa76d48133616296e164539a1cff
Marc Auger <sosaysthecaptain@gmail.com>
2015-12-02 15:27:59 -0600

    MFH restored

--- SpaceTrader/MercenariesVC.swift
+++ SpaceTrader/MercenariesVC.swift
@@ -11,14 +11,18 @@ import UIKit
 class MercenariesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView1: UITableView!
+    @IBOutlet weak var tableView2: UITableView!
+    
     
     var displayItems: [String] = ["first thing", "second thing", "third thing"]
+    var otherArray: [String] = ["one", "two", "three"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView1.registerClass(UITableViewCell.self, forCellReuseIdentifier: "topCell")
+        self.tableView2.registerClass(UITableViewCell.self, forCellReuseIdentifier: "bottomCell")
         
     }
     
@@ -29,13 +33,24 @@ class MercenariesVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
     // TABLE VIEW FUNCTIONS****************************************************************************
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return displayItems.count
+        
+        if tableView == tableView1 {
+            return displayItems.count
+        } else {
+            return otherArray.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")!
-        cell.textLabel?.text = self.displayItems[indexPath.row]
-        return cell
+        if tableView == tableView1 {
+            let cell: UITableViewCell = self.tableView1.dequeueReusableCellWithIdentifier("topCell")!
+            cell.textLabel?.text = self.displayItems[indexPath.row]
+            return cell
+        } else {
+            let cell: UITableViewCell = self.tableView2.dequeueReusableCellWithIdentifier("bottomCell")!
+            cell.textLabel?.text = self.otherArray[indexPath.row]
+            return cell
+        }
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
