36072c26d93ad22daf506e48c4597623714f4809
Marc Auger <sosaysthecaptain@gmail.com>
2015-12-03 14:59:08 -0600

    special-linked newspaper headlines added

---
+++ SpaceTrader/Newspaper.swift
@@ -0,0 +1,58 @@
+//
+//  Newspaper.swift
+//  SpaceTrader
+//
+//  Created by Marc Auger on 12/3/15.
+//  Copyright © 2015 Marc Auger. All rights reserved.
+//
+
+import Foundation
+
+class Newspaper {
+    //var stories: [String]
+    
+    
+}
+
+enum NewsItemID: String {
+    case artifactDelivery = "Scientist Adds Alien Artifact to Museum Collection."
+    case caughtLittering = "Police Trace Orbiting Space Litter to <playerName>."
+    case dragonfly = "Experimental Craft Stolen! Critics Demand Security Review."
+    case dragonflyBaratas = "Investigators Report Strange Craft."
+    case dragonflyDestroyed = "Spectacular Display as Stolen Ship Destroyed in Fierce Space Battle."
+    case dragonflyMelina = "Rumors Continue: Melina Orbitted by Odd Starcraft."
+    case dragonflyRegulas = "Strange Ship Observed in Regulas Orbit."
+    case dragonflyZalkon = "Unidentified Ship: A Threat to Zalkon?"
+    case experimentFailed = "Huge Explosion Reported at Research Facility."
+    case experimentPerformed = "Travelers Report Spacetime Damage, Warp Problems!"
+    case experimentStopped = "Scientists Cancel High-profile Test! Committee to Investigate Design."
+    case experimentArrival = "Travelers Claim Sighting of Ship Materializing in Orbit!"
+    case gemulon = "Editorial: Who Will Warn Gemulon?"
+    case gemulonInvaded = "Alien Invasion Devastates Planet!"
+    case gemulonRescued = "Invasion Imminent! Plans in Place to Repel Hostile Invaders."
+    case captAhabAttacked = "Thug Assaults Captain Ahab!"
+    case captAhabDestroyed = "Destruction of Captain Ahab's Ship Causes Anger!"
+    case captConradAttacked = "Captain Conrad Comes Under Attack By Criminal!"
+    case captConradDestroyed = "Captain Conrad's Ship Destroyed by Villain!"
+    case captHuieAttacked = "Famed Captain Huie Attacked by Brigand!"
+    case captHuieDestroyed = "Citizens Mourn Destruction of Captain Huie's Ship!"
+    case japori = "Editorial: We Must Help Japori!"
+    case japoriDelivery = "Disease Antidotes Arrive! Health Officials Optimistic."
+    case jarekGetsOut = "Ambassador Jarek Returns from Crisis."
+    case scarab = "Security Scandal: Test Craft Confirmed Stolen."
+    case scarabDestroyed = "Wormhole Traffic Delayed as Stolen Craft Destroyed."
+    case scarabHarass = "Wormhole Travelers Harassed by Unusual Ship!"
+    case spaceMonster = "Space Monster Threatens Homeworld!"
+    case spaceMonsterKilled = "Hero Slays Space Monster! Parade, Honors Planned for Today."
+    case wildArrested = "Notorious Criminal Jonathan Wild Arrested!"
+    case wildGetsOut = "Rumors Suggest Known Criminal J. Wild May Come to Kravat!"
+    case sculptureStolen = "Priceless collector's item stolen from home of Geurge Locas!"
+    case sculptureTracked = "Space Corps follows **** with alleged stolen sculpture to ****."
+    case princess = "Member of Royal Family kidnapped!"
+    case princessCentauri = "Aggressive Ship Seen in Orbit Around Centauri"
+    case princessInthara = "Dangerous Scorpion Damages Several Other Ships Near Inthara"
+    case princessQuonos = "Kidnappers Holding Out at Qonos"
+    case princessRescued = "Scorpion Defeated! Kidnapped Member of Galvon Royal Family Freed!"
+    case princessReturned = "Beloved Royal Returns Home!"
+    
+}
