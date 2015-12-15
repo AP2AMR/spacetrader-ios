25927eef0970e9c98d4b0c534d6a4c6b0a103327
Marc Auger <sosaysthecaptain@gmail.com>
2015-12-15 12:02:58 -0600

    menu unbroken

--- SpaceTrader/CommanderStatusVC.swift
+++ SpaceTrader/CommanderStatusVC.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class CommanderStatusVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-
-}
+//class CommanderStatusVC: UITableViewController  {
+//
+//    @IBOutlet weak var tableView: UITableView!
+//    
+//    let section0Data: [(String, String)] = []
+//    let section1Data: [(String, String)] = []
+//    let section2Data: [(String, String)] = []
+//    let section3Data: [(String, String)] = []
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        let cell = UITableViewCell()
+//        
+//        if indexPath.section == 0 {
+//            cell.textLabel?.text = "section 1 entry"
+//        } else if indexPath.section == 1 {
+//            cell.textLabel?.text = "section 2 entry"
+//        } else if indexPath.section == 2 {
+//            cell.textLabel?.text = "section 3 entry"
+//        } else {
+//            cell.textLabel?.text = "section 4 entry"
+//        }
+//        
+//        return cell
+//    }
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 4
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if section == 0 {
+//            return 3 //return section0Data.count
+//        } else if section == 1 {
+//            return 4 //return section1Data.count
+//        } else if section == 2 {
+//            return 4 //return section2Data.count
+//        } else {
+//            return 4 //return section3Data.count
+//        }
+//        
+//        
+//    }
+//
+//}
