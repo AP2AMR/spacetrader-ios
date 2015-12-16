8d0f3dcebf58f0787158c59de4fdf275bf528d4b
Marc Auger <sosaysthecaptain@gmail.com>
2015-12-15 21:54:08 -0600

    ending for night

--- SpaceTrader/QuestsVC.swift
+++ SpaceTrader/QuestsVC.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class QuestsVC: UIViewController {
+class QuestsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
+    // TABLE VIEW METHODS************************************************************************
+    
+    func table
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        <#code#>
+    }
+    
+    
 
 }
