95715d285b117e9f0f1b500591e21204bea92cce
Marc Auger <sosaysthecaptain@gmail.com>
2015-12-17 15:33:45 -0600

    going to airport

--- SpaceTrader/BankQuantityVC.swift
+++ SpaceTrader/BankQuantityVC.swift
@@ -13,6 +13,10 @@ class BankQuantityVC: UIViewController {
     var getVsPayBack: Bool?
     var maxLoan: Int?
 
+    @IBOutlet weak var text1: UILabel!
+    @IBOutlet weak var text2: UILabel!
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
