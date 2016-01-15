c0242e978b8489423141a979a5d7df9f18bc6e0e
Marc Auger <sosaysthecaptain@gmail.com>
2016-01-15 15:18:52 -0600

    autosaves and loads game

--- SpaceTrader/NewGameVC.swift
+++ SpaceTrader/NewGameVC.swift
@@ -11,10 +11,38 @@ import UIKit
 class NewGameVC: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
-        triggerSegue()
+        //triggerSegue()
+        
+        if loadAutosavedGame() {
+            print("autosaved game found")
+            // MAYBE TRIGGER MODAL HERE TO ASK IF USER WANTS TO RESUME GAME?
+            performSegueWithIdentifier("restoredGameSegue", sender: nil)
+        } else {
+            print("no autosaved game found.")
+        }
     }
     
-    @IBAction func triggerSegue() {
-        performSegueWithIdentifier("newGame", sender: self)
+
+    
+    // PERSISTANCE METHODS
+    func documentsDirectory() -> String {
+        let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0]
+        return documentsFolderPath
+    }
+    
+    func fileInDocumentsDirectory(filename: String) -> String {
+        return documentsDirectory().stringByAppendingPathComponent(filename)
+    }
+    
+    func loadAutosavedGame() -> Bool {
+        let path = fileInDocumentsDirectory("autosave.plist")
+        print("path: \(path)")
+        if let autosaveGame = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? SavedGame {
+            player = autosaveGame.savedCommander
+            galaxy = autosaveGame.savedGalaxy
+            return true
+        } else {
+            return false
+        }
     }
 }
